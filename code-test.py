d13a2625314d067d60c8f334538979f293be1fc4
Nikhil Nagarajan <potter.nikhil@gmail.com>
2024-06-17 13:59:06 -0400

    Create code-test.py
    Signed-off-by: Nikhil Nagarajan <potter.nikhil@gmail.com>

---
+++ code-test.py
@@ -0,0 +1,77 @@
+## Code optimizations [WIP]
+
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+def wait_for_files_active(files):
+  """Waits for the given files to be active.
+
+  Some files uploaded to the Gemini API need to be processed before they can be
+  used as prompt inputs. The status can be seen by querying the file's "state"
+  field.
+
+  This implementation uses a simple blocking polling loop. Production code
+  should probably employ a more sophisticated approach.
+  """
+  print("Waiting for file processing...")
+  for name in (file.name for file in files):
+    file = genai.get_file(name)
+    while file.state.name == "PROCESSING":
+      print(".", end="", flush=True)
+      time.sleep(10)
+      file = genai.get_file(name)
+    if file.state.name != "ACTIVE":
+      raise Exception(f"File {file.name} failed to process")
+  print("...all files ready")
+  print()
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 0.95,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+  system_instruction="After every user prompt, summarize with everything above (apart form System Instructions) at the bottom of your reply to provide essential context and information to move ahead with the conversation. Try to follow the following structure:\n\n<Prompt response start>\n\n**Summary:** <Summarized context so far with user>\n<end of response>\n\nUse the summary to remind about important contextual information. If provided append it in the given log file.",
+)
+
+chat_session = model.start_chat(
+  history=[
+    {
+      "role": "user",
+      "parts": [
+        "", ## Prone to injection due to insufficient parameterization. Refer to prompt injection hardening
+      ],
+    },
+    {
+      "role": "model",
+      "parts": [
+        "",
+      ],
+    },
+  ]
+)
+
+response = chat_session.send_message("INSERT_INPUT_HERE")
+
+print(response.text)
